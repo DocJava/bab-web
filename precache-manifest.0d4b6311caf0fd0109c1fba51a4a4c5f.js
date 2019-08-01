37ef16d13d73647372c511bd35b23d7e9362ef73
DJ <djsm00thee3@gmail.com>
2019-07-31 23:14:04 -0700

    Updates

---
+++ precache-manifest.0d4b6311caf0fd0109c1fba51a4a4c5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b6137449fa6b73875a9c2f63f41f6c7",
+    "url": "/bab-web/index.html"
+  },
+  {
+    "revision": "9b83b7311d2b9afc7193",
+    "url": "/bab-web/static/css/2.46439c26.chunk.css"
+  },
+  {
+    "revision": "81bf3d79799c21bb6914",
+    "url": "/bab-web/static/css/main.4d7dd14c.chunk.css"
+  },
+  {
+    "revision": "9b83b7311d2b9afc7193",
+    "url": "/bab-web/static/js/2.885ee55d.chunk.js"
+  },
+  {
+    "revision": "81bf3d79799c21bb6914",
+    "url": "/bab-web/static/js/main.8a4e0ee3.chunk.js"
+  },
+  {
+    "revision": "02b2e42dfe3287d0a20e",
+    "url": "/bab-web/static/js/runtime~main.365fb807.js"
+  }
+]);
